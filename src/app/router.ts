a9dd7b4593b2386e4c68f55e7784f011a49cb9b9
mancinilucas <mancini.lucasg@gmail.com>
2026-01-26 00:47:05 -0300

    refactor: router with error handler

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,15 +1,16 @@
 import { parseUrl } from "./http/parseUrl";
 import { routes } from "./routes";
 import { IncomingMessage, ServerResponse } from "http";
+import { withErrorHandling } from "./http/withErrorHandling";
 
 interface Route {
   method: string;
   path: string;
+  handler: (req: IncomingMessage, res: ServerResponse) => unknown;
 }
 
 export function router(req: IncomingMessage, res: ServerResponse) {
   const { method } = req;
-
   const { pathname } = parseUrl(req.url, req.headers.host);
 
   const route = routes.find(
@@ -19,9 +20,9 @@ export function router(req: IncomingMessage, res: ServerResponse) {
   if (!route) {
     res.statusCode = 404;
     res.setHeader("Content-Type", "text/plain");
-    res.end("Not Found\n");
+    res.end(JSON.stringify({ error: "Not Found" }));
     return;
   }
 
-  route.handler(req, res);
+  withErrorHandling(route.handler, req, res);
 }
